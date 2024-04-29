2e5d0de8434491c46e5598c5b4dfddc171b27ad2
Fadime Aydiner <fadime_aydiner@icloud.com>
2024-04-29 12:38:17 -0500

    Queries from frontnd to backend

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,17 +1,21 @@
-import {getJobs} from './db/jobs.js';
+import {getJobs,getJob,getJobsByCompany} from './db/jobs.js';
 import {getCompany} from './db/companies.js';
 export const resolvers = {
     Query:{
+        job:(_root,{id})=>getJob(id),
         jobs: ()=> getJobs(),
-         
-        },
-        Job:{
+        company:(_root,{id})=>getCompany(id),
+    },
+    Company:{
+        jobs:(company)=>getJobsByCompany(company.id),
+    },
+    Job:{
             company: (job)=> getCompany(job.companyId),
             date:(job)=> toIsoDate(job.createdAt),
-        },
+    },
         
         
-    };
+};
 
     function toIsoDate(value){
         return value.slice(0,'yyyy-MM-dd'.length);
